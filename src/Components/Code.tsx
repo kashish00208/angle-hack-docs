b8bbcabfddf69d735f5b4a376502db00947e6e37
kashish00208 <kgupta7827@gmail.com>
2025-08-24 19:28:37 +0530

    minor changes

--- src/Components/Code.tsx
+++ src/Components/Code.tsx
@@ -6,7 +6,7 @@ interface CodeProps {
 
 const Code: React.FC<CodeProps> = ({ children }) => {
   return (
-    <pre className="bg-gray-300 text-black p-4 rounded-xl overflow-x-auto mb-4 my-4">
+    <pre className="bg-gray-300 text-black p-4 rounded-xl overflow-x-auto mb-4 my-4 text-sm">
       <code>{children}</code>
     </pre>
   );
